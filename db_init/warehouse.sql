d5b00e6c77a5b1afdaa28aef70825870e69c8827
ayoub <bousserhaneayoub@gmail.com>
2026-05-04 09:37:05 +0100

    added a time dimention table

--- db_init/warehouse.sql
+++ db_init/warehouse.sql
@@ -2,8 +2,19 @@
 CREATE TABLE IF NOT EXISTS bi_schema.dim_localisation AS 
 SELECT DISTINCT city FROM clean.annonces;
 
+CREATE TABLE IF NOT EXISTS bi_schema.dim_temps AS 
+SELECT DISTINCT 
+    TO_CHAR(scraped_at, 'YYYYMMDD')::INTEGER as date_key,
+    scraped_at::DATE as full_date,
+    EXTRACT(YEAR FROM scraped_at) as year,
+    EXTRACT(MONTH FROM scraped_at) as month,
+    EXTRACT(DAY FROM scraped_at) as day,
+    TO_CHAR(scraped_at, 'Month') as month_name,
+    EXTRACT(QUARTER FROM scraped_at) as quarter
+FROM staging.raw_annonces;
+
 CREATE TABLE IF NOT EXISTS bi_schema.fact_annonces AS 
-SELECT id, price_dh, surface_m2, rooms, bathrooms, city, price_per_m2 FROM clean.annonces;
+SELECT id, TO_CHAR(scraped_at, 'YYYYMMDD')::INTEGER as date_key, price_dh, surface_m2, rooms, bathrooms, city, price_per_m2 FROM clean.annonces;
 
 -- 6. ML SCHEMA: One Big Table (OBT) for Features
 CREATE TABLE IF NOT EXISTS ml_schema.obt_annonces AS 
